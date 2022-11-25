ea302832af4488e3cc0ef0de6433322245d236f6
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-25 19:23:20 +0100

    ??

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,6 +3,6 @@ import { setupCounter } from './counter'
 import { login_and_print_logged_user } from './fetchexample'
 
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
-// login_and_print_logged_user("siema", "test");
+login_and_print_logged_user("test", "test");
