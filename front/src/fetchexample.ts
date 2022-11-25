b8cd35e3ddd174b4195aca3ccf6705b23dfa3701
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-25 14:58:30 +0100

    .

--- front/src/fetchexample.ts
+++ front/src/fetchexample.ts
@@ -18,14 +18,16 @@ export const login_and_print_logged_user = async (username: string, password: st
         "/login", 
         "POST", 
         { username, password },
-    ).then(res => res.text())
+    )
+    .then(res => res.text())
+    .catch(() => console.error("Problem with logging in"));    
 
     //fetch currently logged in user
     let user = await fetch_api(
         "/user",
         "GET",
         undefined,
-        access_token
+        access_token ?? undefined
     )
     .then(res => res.json())
     .catch(() => console.error("Not logged in"));
