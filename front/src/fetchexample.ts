78a91f63aa70427f13611aa923fbeeb4c98c6ce5
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-25 19:38:17 +0100

    accesstoken type

--- front/src/fetchexample.ts
+++ front/src/fetchexample.ts
@@ -1,33 +1,35 @@
+import { AccessToken } from "./types";
+
 export const fetch_api = (
     route: string, 
     method: string, 
     json: object | undefined = undefined,
-    access_token: string | undefined = undefined, 
+    access_token: AccessToken | undefined = undefined, 
 ) => fetch(`http://127.0.0.1:8000${route}`, {
     ...(json && { body: JSON.stringify(json) }),
     headers: {
         ...(json && { "Content-Type": "application/json" }),
-        ...(access_token && { "Authorization": `Bearer: ${access_token}` }),
+        ...(access_token && { "Authorization": `Bearer: ${access_token.token}` }),
     },
     method: method,
 })
 
-export const login_and_print_logged_user = async (username: string, password: string) => {
+export const login_and_print_logged_user = async (username: string, password: string, token: AccessToken) => {
     //login user example
-    let access_token = await fetch_api(
+    token.token = await fetch_api(
         "/login", 
         "POST", 
         { username, password },
     )
     .then(res => res.text())
-    .catch(() => console.error("Problem with logging in"));    
+    .catch(() => console.error("Problem with logging in")) ?? "";    
 
     //fetch currently logged in user
     let user = await fetch_api(
         "/user",
         "GET",
         undefined,
-        access_token ?? undefined
+        token ?? undefined
     )
     .then(res => res.json())
     .catch(() => console.error("Not logged in"));
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,8 +1,21 @@
 import './index.css'
 import { setupCounter } from './counter'
-import { login_and_print_logged_user } from './fetchexample'
+import { fetch_api, login_and_print_logged_user } from './fetchexample'
 
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let token = { token: "" };
 
-login_and_print_logged_user("test", "test");
+
+login_and_print_logged_user("test", "test", token);
+
+let users = await fetch_api(
+    "/users",
+    "GET",
+    undefined,
+    token ?? undefined
+)
+.then(res => res.json())
+.catch(() => console.error("?"));
+
+console.log(users);
