0e3ebfb32d4e5b6578dff9c9e002c37dd04feee0
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-27 18:29:43 +0100

    new types + sync backend (like/dislike fix)

--- front/src/types.ts
+++ front/src/types.ts
@@ -6,6 +6,15 @@ export class AccessToken {
     _marker: number = 0;
 };
 
+export type Question = {
+    id: number,
+    content: string,
+    likes: number,
+    asked_id: number,
+    asker_id: number | undefined,
+    asked_at: string //Date
+};
+
 export type Answer = {
     id: number,
     question_id: number,
@@ -15,25 +24,26 @@ export type Answer = {
     last_edit_at: string, //Date
 };
 
+export type QuestionWithAnswer = {
+    question: Question,
+    answer: Answer | null
+}
+
 export type User = {
     id: number,
     username: string,
     follower_count: number,
     following_count: number,
     bio: string,
-}
+};
 
 export type Like = {
     liker_id: number,
-    like_type: "QDislike" | "QLike" | "ADislike" | "ALike",
+    like_type: "QuestionLike" | "QuestionDislike" | "AnswerLike" | "AnswerDislike",
     resource_id: number,
-}
+};
 
-export type Question = {
-    id: number,
-    content: string,
-    likes: number,
-    asked_id: number,
-    asker_id: number | undefined,
-    asked_at: string //Date
-}
+export type UserWithLikes = {
+    user: User,
+    likes: Like[]
+};
