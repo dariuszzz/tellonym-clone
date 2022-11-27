fe38cec0e3ebcce571f7f3fed4a9ae821e102963
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-27 13:42:23 +0100

    refactor fetch wrapper + sync backend

--- front/src/fetchexample.ts
+++ front/src/fetchexample.ts
@@ -1,47 +1,42 @@
 import { AccessToken } from "./types";
 
+const SERVER_URL = "http://127.0.0.1:8000";
 
+//Doesnt try to refresh token after an unauthorized request
+export const fetch_api_raw = async (
+    route: string, 
+    method: string, 
+    json: object | undefined = undefined,
+    access_token: AccessToken | undefined = undefined,
+) => fetch(`${SERVER_URL}${route}`, {
+    ...(json && { body: JSON.stringify(json) }),
+    headers: {
+        ...(json && { "Content-Type": "application/json" }),
+        ...(access_token && { "Authorization": `Bearer: ${access_token.token}` }),
+    },
+    credentials: "include",
+    method: method,
+});
 
-// Wrapper do fetcha ktory fetchuje nowy token jezeli stary sie juz skonczyl
-// dzieki temu nie wylogowywuje po przejsciu miedzy stronami :)))))
+//Fetch wrapper which automatically tries to refresh the access token on unauthorized requests
 export const fetch_api = async (
     route: string, 
     method: string, 
     json: object | undefined = undefined,
     access_token: AccessToken | undefined = undefined, 
 ) => {
-    let result = await fetch(`http://127.0.0.1:8000${route}`, {
-        ...(json && { body: JSON.stringify(json) }),
-        headers: {
-            ...(json && { "Content-Type": "application/json" }),
-            ...(access_token && { "Authorization": `Bearer: ${access_token.token}` }),
-        },
-        credentials: "include",
-        method: method,
-    });
+    let result = await fetch_api_raw(route, method, json, access_token);
 
-    if (result.status == 400 && access_token) {
-        access_token.token = await fetch(`http://127.0.0.1:8000/refresh`, {
-            credentials: "include",
-            method: "POST", 
-        }).then(res2 => {
-            if (!res2.ok) {
-                throw new Error("Need to log in");
-            }
+    //Only if unauthorized (no token) and the token was passed in
+    if (result.status == 401 && access_token) {
+        
+        access_token.token = await fetch_api_raw("/refresh", "POST")
+            .then(res => {
+                if (!res.ok) throw new Error("Not logged in")
+                else return res.text();
+            })
 
-            return res2.text();
-        })
-
-        return await fetch(`http://127.0.0.1:8000${route}`, {
-            ...(json && { body: JSON.stringify(json) }),
-            headers: {
-                //Token should be available and needed since a refresh was needed and happened
-                "Authorization": `Bearer: ${access_token.token}`,
-                ...(json && { "Content-Type": "application/json" }),
-            },
-            credentials: "include",
-            method: method,
-        })
+        return await fetch_api_raw(route, method, json, access_token);
     }
 
     return result;
