a4caa90e1d31f29dad78a2d0fe73836289ac395e
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-29 01:18:26 +0100

    avatar scaling + sync backend + pfp preview

--- front/src/edit_profile.ts
+++ front/src/edit_profile.ts
@@ -1,6 +1,6 @@
 import "./index.css"
 import { AccessToken, UserWithLikes } from "./types";
-import { edit_profile_data, fetch_api } from "./utils";
+import { edit_profile_data, fetch_api, SERVER_URL } from "./utils";
 
 const token = new AccessToken();
 
@@ -15,6 +15,17 @@ const my_user: UserWithLikes | undefined = await fetch_api(
 //redirect na login jak nie ma usera
 if (!my_user) window.location.href = `${window.location.origin}/login.html`
 
+const pfp_preview = <HTMLImageElement>document.getElementById("pfpPreview")!;
+pfp_preview.src = `${SERVER_URL}/pfps/${my_user?.user.id}.png`;
+
+const pfp_input = <HTMLInputElement>document.getElementById("pfpInput")!;
+
+pfp_input.addEventListener("change", function () {
+    if (this.files?.[0]) {
+        const url = URL.createObjectURL(this.files[0]);
+        pfp_preview.src = url;
+    }
+});
 
 const edit_profile_form = <HTMLFormElement>document.getElementById('editProfileForm')!;
 
